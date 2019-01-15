d61a66be5ce8949d50dd8edb3447ed03b58db889
local <local>
2019-01-15 18:49:03 -0500

    updated

---
+++ problems/DIV1_EASY/SRM_500_DIV_1/MafiaGame.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MafiaGame {
+  public:
+  double probabilityToLose(int N, vector <int> decisions) {
+		
+  }
+};
+// BEGIN CUT HERE
+int main(){ 
+  
+  return 0;
+}
+// END CUT HERE
